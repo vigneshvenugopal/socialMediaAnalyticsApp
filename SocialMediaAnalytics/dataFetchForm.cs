a240aa0cb9260cdc185c56a5ce54c01c93b5773b
Vignesh Venugopal <vickyv@live.in>
2017-04-15 16:18:03 +0530

    Adding the official Handle field

--- SocialMediaAnalytics/dataFetchForm.cs
+++ SocialMediaAnalytics/dataFetchForm.cs
@@ -35,20 +35,20 @@ namespace SocialMediaAnalytics
             csv.AppendLine(line_7);
             var line_8 = string.Format("{0},{1}", "Hash Tag", twitterHashTag.Text == null ? "0" : twitterHashTag.Text);
             csv.AppendLine(line_8);
-            //var line_9 = string.Format("{0},{1}", "Hash Tag", twitterOfficialHandle);
-            //csv.AppendLine(line_9);
-            var line_9 = string.Format("{0},{1}", "Tweets From", twitterStartDate.Text == null ? "0" : DateTime.Parse(twitterStartDate.Text).ToString("yyyy-MM-dd"));
+            var line_9 = string.Format("{0},{1}", "Official Handle", twitterOfficialHandle);
             csv.AppendLine(line_9);
-            var line_10 = string.Format("{0},{1}", "Tweets Till", twitterEndDate.Text == null ? "0" : DateTime.Parse(twitterEndDate.Text).ToString("yyyy-MM-dd"));
+            var line_10 = string.Format("{0},{1}", "Tweets From", twitterStartDate.Text == null ? "0" : DateTime.Parse(twitterStartDate.Text).ToString("yyyy-MM-dd"));
             csv.AppendLine(line_10);
-            var line_11 = string.Format("{0},{1}", "Tweet Count", 1000);
+            var line_11 = string.Format("{0},{1}", "Tweets Till", twitterEndDate.Text == null ? "0" : DateTime.Parse(twitterEndDate.Text).ToString("yyyy-MM-dd"));
             csv.AppendLine(line_11);
-            var line_12 = string.Format("{0},{1}", "Video ID", youtubeVideoID.Text == null ? "0" : youtubeVideoID.Text);
+            var line_12 = string.Format("{0},{1}", "Tweet Count", 1000);
             csv.AppendLine(line_12);
-            var line_13 = string.Format("{0},{1}", "Facebook API Key", "477873422398335|2b9be6bc09b2c6544bb827342ee93e8d");
+            var line_13 = string.Format("{0},{1}", "Video ID", youtubeVideoID.Text == null ? "0" : youtubeVideoID.Text);
             csv.AppendLine(line_13);
-            var line_14 = string.Format("{0},{1}", "YouTube API Key", "AIzaSyA5lmEsMapTfMu0l2TDS_o6n3WzTYnqUDE");
+            var line_14 = string.Format("{0},{1}", "Facebook API Key", "477873422398335|2b9be6bc09b2c6544bb827342ee93e8d");
             csv.AppendLine(line_14);
+            var line_15 = string.Format("{0},{1}", "YouTube API Key", "AIzaSyA5lmEsMapTfMu0l2TDS_o6n3WzTYnqUDE");
+            csv.AppendLine(line_15);
 
             File.WriteAllText(fileLocation, csv.ToString());
         }
